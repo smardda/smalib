7b1618b612b6ce6500d872d05e9d4168acd59fb2
Wayne Arter <w.arter@btinternet.com>
2019-06-25 07:53:42 +0100

    improve vtktfm ability to extract geometry

--- f95/bods_h.f90
+++ f95/bods_h.f90
@@ -5,7 +5,9 @@ module bods_h
   type, public :: bods_t
    !! bods data type
      integer(ki4), dimension(:), allocatable :: list !< list of bods
+     integer(ki4), dimension(:), allocatable :: mark !< used to mark bods
      integer(ki4)  :: nbod !< no of bods
+     integer(ki4)  :: nmark !< no of marks
      integer(ki4), dimension(:), allocatable :: indx !< index of bods
      integer(ki4)  :: nindx !< number in index
      integer(ki4)  :: maxindx !< allocated size of index array
